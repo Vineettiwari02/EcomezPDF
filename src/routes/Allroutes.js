5bd5cffec8d15b60205ee06d5dc746d2e656c06a
Vineet Tiwari <43114880+Vineettiwari02@users.noreply.github.com>
2024-08-03 10:35:32 +0530

    protected routes

--- src/routes/Allroutes.js
+++ src/routes/Allroutes.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { Cartpage, Homepage,Login,ProductDetail,ProductList, Register } from '../pages'
+import { Cartpage, Homepage,Login,ProductDetail,ProductList, Register } from '../pages';
+import { ProtectedRoute } from './ProtectedRoute';
 export const Allroutes = () => {
     return (
         <>
             <Routes>
                 <Route path="/" element={<Homepage/>}/>
                 <Route path="products" element={<ProductList/>}/>
-                <Route path="cart" element={<Cartpage/>}/>
                 <Route path="products/:id" element={<ProductDetail/>}/>
                 <Route path="login" element={<Login/>}/>
                 <Route path="register" element={<Register/>}/>
+
+                <Route path="cart" element={<ProtectedRoute><Cartpage/></ProtectedRoute>}/>
             </Routes>
         </>
     )
